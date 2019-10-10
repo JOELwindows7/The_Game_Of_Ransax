802ccdd4786f83d8af6c9314afe6f8dba4184ab6
Joel Robert Justiawan <joelwindows7@gmail.com>
2019-10-10 10:36:42 +0700

    We've done the Racing research
    but we got to wait for the assets.
    
    wait! how to I make racing games? this one is not my idea, but one / some of my employee(s)!!
    
    I just know the shooting gallery game.
    
    uh.. plns help. the time is shrinking faster than Time.deltaTime!
    someone has increased the Time.scale illegally!

---
+++ Assets/Scripts/Brumer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brumer : MonoBehaviour
+{
+
+    public float speeds = 10f;
+    public float steers = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Vertical") < -.1f || Input.GetAxis("Vertical") > .1f)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speeds * Mathf.Clamp(Input.GetAxis("Vertical"), -1f, 1f), Space.Self);
+        }
+        if(Input.GetAxis("Horizontal") < -.1f || Input.GetAxis("Horizontal") > .1f)
+        {
+            transform.Rotate(-Vector3.forward * steers * Mathf.Clamp(Input.GetAxis("Horizontal"),-1f,1f));
+
+        }
+    }
+}
