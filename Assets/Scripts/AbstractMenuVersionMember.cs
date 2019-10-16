a3485b5252625942e6b6faeb06d82a684e5a5d89
Joel Robert Justiawan <joelwindows7@gmail.com>
2019-10-16 14:13:56 +0700

    lazie
    I got UI but I fell tired by hectics

---
+++ Assets/Scripts/AbstractMenuVersionMember.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractMenuVersionMember: MonoBehaviour
+{
+    public enum MenuScreenLists { MainMenu, Setting, Shop, Equip, GameLobby, GamePlay };
+    public MenuScreenLists MenuScreenIsAt;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BuiltInMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuiltInMenu : MonoBehaviour
+{
+    public enum MenuScreenLists { MainMenu,Setting,Shop,Equip,GameLobby,GamePlay};
+    public MenuScreenLists MenuScreenRightNow;
+    private AbstractMenuVersionMember[] MenuVersionMembers;
+    private void Awake()
+    {
+        //foreach(GameObject MenuMembers in transform.ch)
+        //{
+        //    MenuVersionMembers = MenuMembers.GetComponent<AbstractMenuVersionMember>();
+        //}
+    }
+    public enum MenuVersion { Custom,Horizontal,Vertical};
+    public MenuVersion MenuSelectedVersion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
