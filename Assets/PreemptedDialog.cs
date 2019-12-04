c9a02607906c83fcc1cae2851763a633053c5de1
Joel Robert Justiawan <joelwindows7@gmail.com>
2019-12-04 15:23:55 +0700

    Greangd New
    Shrunk Balloon target size scale
    
    Update!
    - Made Target Balon do not collide each other. by set Balon Layer to layer "Balon" and uncheck itself collision matrix
    - Spawn Pause Menu!!!
    
    another update:
    - Pause Resume with Time scale Zero and return from temp time scale

--- Assets/PreemptedDialog.cs
+++ Assets/PreemptedDialog.cs
@@ -1,9 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PreemptedDialog : MonoBehaviour
 {
+    public enum WhichMode { Paused, Game_Over};
+    public WhichMode PreemptedMode;
+
+    public ParlorSpecificUI itselfEngine;
+    [SerializeField] bool isGamePaused = false;
+    [SerializeField] bool isGameStarted = false;
+
+    public TextMeshProUGUI PreemptTitle;
+    public TMP_InputField PlayerHighScored;
+    public TMP_InputField CurrentHighScored;
+    public Button RetryResumeButton;
+    public Button QuittoButton;
+
+    public bool IsGamePaused { get => isGamePaused; set => isGamePaused = value; }
+    public bool IsGameStarted { get => isGameStarted; set => isGameStarted = value; }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +31,63 @@ public class PreemptedDialog : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (PreemptedMode)
+        {
+            default:
+                break;
+            case WhichMode.Paused:
+                PreemptTitle.text = "PAUSED";
+                RetryResumeButton.GetComponentInChildren<Text>().text = "Resume";
+                break;
+            case WhichMode.Game_Over:
+                PreemptTitle.text = "GAME OVER";
+                RetryResumeButton.GetComponentInChildren<Text>().text = "Retry";
+                break;
+        }
+    }
+
+    public void PressRetryResume()
+    {
+        //if (RetryResumeButton)
+        //{
+        //    if (itselfEngine)
+        //    {
+        //        if (itselfEngine.isGamePaused)
+        //        {
+
+        //        } else
+        //        {
+
+        //        }
+        //    }
+        //}
+        if (IsGamePaused)
+        {
+            //Resume Button
+            if (itselfEngine)
+            {
+                //Resynne funtuon
+                itselfEngine.ResumeTheGame();
+                gameObject.SetActive(false);
+            }
+        } else
+        {
+            if (!IsGameStarted)
+            {
+                //Retry Button
+                if (itselfEngine)
+                {
+                    //Retrye FUnction
+
+
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void PressQuitto()
+    {
+
     }
 }
--- Assets/Scripts/ParlorSpecificUI.cs
+++ Assets/Scripts/ParlorSpecificUI.cs
@@ -6,13 +6,19 @@ using TMPro;
 
 public class ParlorSpecificUI : MonoBehaviour
 {
-    [Range(0f, 100f)] public float HPLevel;
+    [Range(0f, 100f)] public float HPLevel = 100f;
     public int AmmoNumber;
     public float ScoreNumber;
+
+    public bool isGamePaused = false;
 
     public Slider HPBar;
     public TextMeshProUGUI AmmoText;
     public TextMeshProUGUI ScoreText;
+
+    public AbstractGameplayUI AbstractGameplayUI;
+    public AbstractShopUI AbstractShopUI;
+    public PreemptedDialog preemptedDialog;
 
     // Start is called before the first frame update
     void Start()
@@ -26,5 +32,53 @@ public class ParlorSpecificUI : MonoBehaviour
         HPBar.value = HPLevel;
         AmmoText.text = AmmoNumber.ToString();
         ScoreText.text = ScoreNumber.ToString();
+    }
+
+    public void PauseTheGame()
+    {
+        if (preemptedDialog)
+        {
+            preemptedDialog.IsGamePaused = true;
+        }
+        SpawnPreemptedDialogAs(PreemptedDialog.WhichMode.Paused);
+        FreezeTime();
+        isGamePaused = true;
+    }
+
+    public void ResumeTheGame()
+    {
+        if (preemptedDialog)
+        {
+            preemptedDialog.IsGamePaused = false;
+        }
+        UnFreezeTime();
+        isGamePaused = false;
+    }
+
+    [SerializeField] float tempTimeScale;
+    public void FreezeTime()
+    {
+        tempTimeScale = Time.timeScale;
+        Time.timeScale = 0f;
+    }
+
+    public void UnFreezeTime()
+    {
+        Time.timeScale = tempTimeScale;
+        //Time.timeScale = 1f;
+    }
+
+    void SpawnPreemptedDialogAs()
+    {
+        if (preemptedDialog)
+        {
+            preemptedDialog.gameObject.SetActive(true);
+        }
+    }
+
+    public void SpawnPreemptedDialogAs(PreemptedDialog.WhichMode WhatMode)
+    {
+        preemptedDialog.PreemptedMode = WhatMode;
+        SpawnPreemptedDialogAs();
     }
 }
